726a3b67b3c1ed3a9f8353fce646ccb8dc7de190
Centar za održive zajednice <dusko.medic@gmail.com>
2020-11-17 01:32:53 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,68 +5,77 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  siteMetadata: {
-    title: `Woo - Free Responsive HTML5/CSS3 Template`,
-    author: {
-      name: `Joe Bloggs`,
+    /* Your site config here */
+    siteMetadata: {
+        title: `Centar za održive zajednice`,
+        author: {
+            name: `Duško Medić`,
+        },
+        description: `Websajt nevladine organizacije iz Novog Sada`,
+
+        /* Choose and arrange the sections to be displayed on the landing page */
+        sections: [
+            `hero`,
+            `features`,
+            // `pricing`,
+            `testimonials`,
+            `call-to-action`,
+            // `screenshots`,
+            // `subscribe`
+        ],
+
+        /* Configure the navigation menu */
+        menuItems: [
+            { path: 'features', label: 'Izdvajamo' },
+            // { path: 'pricing', label: 'Publikacije' },
+            // { path: 'screenshots', label: 'Projekti' },
+            { path: 'testimonials', label: 'O nama' },
+            // { path: 'subscribe', label: 'Subscribe' },
+        ],
+
+        /* Provide social media accounts. Remove or add new services. (The value 'fa' is the name of FontAwesome icon to display. Choose from here: https://fontawesome.com/cheatsheet/free/brands ) */
+        social: [{
+                service: `Facebook`,
+                url: `https://www.facebook.com/PolitikeEUiSRBIJAKlimaiEkologija`,
+                fa: `facebook`
+            },
+            {
+                service: `Twitter`,
+                url: `https://twitter.com/prijateljizs`,
+                fa: `twitter`
+            },
+            {
+                service: `GitHub`,
+                url: `https://github.com/duskomedic`,
+                fa: `github`
+            },
+        ],
+
+        /* Path to favicon and logo (located inside the 'static' folder) */
+        favicon: `favicon.png`,
+        logo: `/images/logo.png`,
+
+        /* Logo and text to show in footer */
+        footer: {
+            logo: `/images/footer-logo.png`,
+            text: `Centar za odrzive zajednice (COZ) / Center for Sustainable Communities (CSC)
+            Matični broj: 28046057
+            PIB: 107077984
+            Datum osnivanja: 24.02.2011.
+            Delatnost: 9949 – Delatnost ostalih organizacija na bazi učlanjenja`,
+        },
+
+        /* Address and contact info to show in footer */
+        address: {
+            line1: `Jerneja Kopitara 40c`,
+            line2: `21000 Novi Sad`,
+            line3: `Republika Srbija`,
+        },
+        contacts: [
+            { text: `+381606466205`, url: `+381606466205` },
+            { text: `+381216466205`, url: `tel:+381216466205` },
+            { text: `prijateljizs@gmail.com`, url: `mailto:prijateljizs@gmail.com` },
+        ],
     },
-    description: `Woo is a clean, modern, landing page site template.`,
-
-    /* Choose and arrange the sections to be displayed on the landing page */
-    sections: [
-      `hero`,
-      `features`,
-      `pricing`,
-      `call-to-action`,
-      `screenshots`,
-      `testimonials`,
-      `subscribe`
-    ],
-
-    /* Configure the navigation menu */
-    menuItems: [
-      {path: 'features', label: 'Features'},
-      {path: 'pricing', label: 'Pricing'},
-      {path: 'screenshots', label: 'Screenshots'},
-      {path: 'testimonials', label: 'Testimonials'},
-      {path: 'subscribe', label: 'Subscribe'},
-    ],
-
-    /* Provide social media accounts. Remove or add new services. (The value 'fa' is the name of FontAwesome icon to display. Choose from here: https://fontawesome.com/cheatsheet/free/brands ) */
-    social: [
-      { service: `Facebook`,
-        url : `https://facebook.com/styleshout`,
-        fa: `facebook` },
-      { service: `Twitter`,
-        url : `https://twitter.com/styleshout`,
-        fa: `twitter` },
-      { service: `GitHub`,
-        url : `https://github.com/desmukh/gatsby-starter-woo`,
-        fa: `github` },
-    ],
-
-    /* Path to favicon and logo (located inside the 'static' folder) */
-    favicon: `favicon.png`,
-    logo: `/images/logo.png`,
-
-    /* Logo and text to show in footer */
-    footer: {
-      logo: `/images/footer-logo.png`,
-      text: `This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.`,
-    },
-
-    /* Address and contact info to show in footer */
-    address: {
-      line1: `1600 Amphitheatre Parkway`,
-      line2: `Mountain View, CA`,
-      line3: `94043 US`,
-    },
-    contacts: [
-      { text: `647.343.8234`, url: `tel:6473438234` },
-      { text: `123.456.7890`, url: `tel:1234567890`},
-      { text: `someone@woosite.com`, url: `mailto:someone@woo.com`},
-    ],
-  },
-  plugins: [ `gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`, `react-scrollspy` ],
+    plugins: [`gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`, `react-scrollspy`],
 }
